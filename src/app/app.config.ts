9a2932aa57f0fe6ed00224b859d3b342654b5576
seyitalisoy <soyseyitali23@gmail.com>
2025-06-14 09:58:44 +0300

    toastr and animations configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 2000,
+      positionClass : 'toast-bottom-right',
+    })
   ]
 };
