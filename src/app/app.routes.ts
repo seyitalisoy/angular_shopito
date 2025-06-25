ece64bc70ea63f5a39f2d08b60657ef68a8626d5
seyitalisoy <soyseyitali23@gmail.com>
2025-06-25 15:15:44 +0300

    routes modified for loginComponent and loginGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
     {path:'',component: ProductComponent},
     {path:'products',component:ProductComponent},
     {path:'products/category/:categoryId',component:ProductComponent},
-    {path:'products/add',component:ProductAddComponent}
+    {path:'products/add',component:ProductAddComponent,canActivate:[loginGuard]},
+    {path:'login',component:LoginComponent}
 ];
