c95b7a4918b0cd0d22bd2555972a1ef89d8bdc1b
seyitalisoy <soyseyitali23@gmail.com>
2025-06-25 15:13:21 +0300

    interceptors configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 import { provideToastr } from 'ngx-toastr';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
     provideToastr({
-      timeOut: 2000,
+      timeOut: 5000,
       positionClass : 'toast-bottom-right',
     })
   ]
