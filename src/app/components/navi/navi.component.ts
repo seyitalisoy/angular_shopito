12f038590beba7618f71f8e9c5e759b48720c69a
seyitalisoy <soyseyitali23@gmail.com>
2025-07-10 14:59:01 +0300

    routerLink added to navbar

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CartSummaryComponent } from "../cart-summary/cart-summary.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navi',
-  imports: [CartSummaryComponent],
+  imports: [CartSummaryComponent, RouterLink],
   templateUrl: './navi.component.html',
   styleUrl: './navi.component.css'
 })
