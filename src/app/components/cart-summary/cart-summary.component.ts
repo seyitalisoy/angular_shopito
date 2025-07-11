4af02d7c3c91d5e3dc703ffbd56aec862b218eb1
seyitalisoy <soyseyitali23@gmail.com>
2025-07-11 15:01:07 +0300

    BehaviorSubject and localStorage using for Cart

--- src/app/components/cart-summary/cart-summary.component.ts
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { Product } from '../../models/product';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart-summary',
@@ -11,17 +12,23 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './cart-summary.component.html',
   styleUrl: './cart-summary.component.css'
 })
-export class CartSummaryComponent implements OnInit{
+export class CartSummaryComponent implements OnInit,OnDestroy{
   cartItems : CartItem[]=[];
-  
+  private subscription : Subscription;
+
+
   constructor(private cartService:CartService,private toastrService: ToastrService) {    
   }
+  
   ngOnInit(): void {
-    this.getCart();
+    this.subscription = this.cartService.cart.subscribe(items => {
+      this.cartItems = items;
+    });
+    // this.getCart();
   }
 
   getCart(){
-    this.cartItems = this.cartService.list();
+    this.cartItems = this.cartService.getCart();
   }
 
   removeFromCart(product:Product){
@@ -29,4 +36,11 @@ export class CartSummaryComponent implements OnInit{
     this.toastrService.error("Sepetten silindi",product.productName)
   }
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
+
+
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,33 +2,69 @@ import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { CartItems } from '../models/cartItems';
 import { CartItem } from '../models/cartItem';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  private storageKey = 'cartItems';
+
+  private cartSubject = new BehaviorSubject<CartItem[]>(this.getCart());
+
+  cart = this.cartSubject.asObservable();
+
   constructor() { }
 
-  addToCart(product:Product){
-    let item = CartItems.find(c=>c.product.productId===product.productId);
 
-    if (item) {
-      item.quantity+=1;
+  private saveCart(cart: CartItem[]): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(cart));
+    this.cartSubject.next(cart);
+  }
+
+  // addToCart(product:Product){
+  //   let item = CartItems.find(c=>c.product.productId===product.productId);
+
+  //   if (item) {
+  //     item.quantity+=1;
+  //   }else{
+  //     let cartItem = new CartItem();
+  //     cartItem.product = product;
+  //     cartItem.quantity = 1;
+  //     CartItems.push(cartItem);
+  //   }
+  // }
+  addToCart(product: Product): void {
+    const cart: CartItem[] = this.getCart();
+    const item = cart.find(c=> c.product.productId === product.productId);
+    if(item){
+      item.quantity +=1;
     }else{
-      let cartItem = new CartItem();
-      cartItem.product = product;
-      cartItem.quantity = 1;
-      CartItems.push(cartItem);
+      cart.push({product,quantity:1});
     }
+    this.saveCart(cart);
   }
 
-  list(): CartItem[]{
-    return CartItems;
+  // list(): CartItem[]{
+  //   return CartItems;
+  // }
+  getCart() : CartItem[]{
+    const cartJson = localStorage.getItem(this.storageKey);
+    return cartJson ? JSON.parse(cartJson) : [];
   }
 
-  removeFromCart(product: Product){
-    let item = CartItems.find(c=>c.product.productId===product.productId);
-    CartItems.splice(CartItems.indexOf(item),1);
+  // removeFromCart(product: Product){
+  //   let item = CartItems.find(c=>c.product.productId===product.productId);
+  //   CartItems.splice(CartItems.indexOf(item),1);
+  // }
+  removeFromCart(product: Product): void{
+    let cart: CartItem[] = this.getCart();
+    cart = cart.filter(item=> item.product.productId !== product.productId);
+    this.saveCart(cart);
+  }
+
+  clearCart() : void{
+    this.saveCart([]);
   }
 }
